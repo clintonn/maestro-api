2ef7a2e79e55b16a1c88caddd36a5dc5edf44839
Clinton Nguyen <clinton.ngyn@gmail.com>
2017-02-22 10:34:32 -0500

    Add destroy votes action

--- app/controllers/api/v1/votes_controller.rb
+++ app/controllers/api/v1/votes_controller.rb
@@ -5,14 +5,18 @@ class Api::V1::VotesController < Api::V1::ApplicationController
   def create
     @trail = Trail.find(params[:trail_id])
     @user = User.find(params[:user_id])
-    vote_up_trail unless user_already_voted_for_trail
+    user_already_voted_for_trail ? downvote_trail : vote_up_trail
     render json: @trail, include: [:author, sections: [:resources]]
   end
 
   private
 
   def vote_up_trail
-    @user.voted_trails << @trail
+    @user.voted_trails << @trail if !@user.voted_trails.include?(@trail)
+  end
+
+  def downvote_trail
+    @user.votes.where(trail_id: @trail.id)[0].destroy
   end
 
   def user_already_voted_for_trail
